9802531a2cab87af3c59802f8fbaa18b2078baff
Isaac Ajayi <ajayioisaac@gmail.com>
2022-09-08 09:18:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,11 @@
  * main - print the string in the printf function
  *
  * Description: using the main function
- * This program prints "whatever is in the printf function
+ * This program prints "Programming is like abuilding a multilingual puzzle
  * Return: 0
- */
+ * */
 int main(void)
 {
-	printf("\"with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return(0);
 }
