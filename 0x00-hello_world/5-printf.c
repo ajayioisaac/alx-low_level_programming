6290c1db4e3e3a418f3ce60cf6d6602df1c8f1e7
Isaac Ajayi <ajayioisaac@gmail.com>
2022-09-08 08:56:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the printf function
+ *
+ * Description: using the main function
+ * This program prints whatever is in the printf function
+ * Returns: 0
+ */
+int main(void)
+{
+	printf(with proper grammar, but the outcome is a piece of art, \n");
+	return(0);
+}
