b5cfd417d60421dd82204a56ad39462d9ea0af2b
Isaac Ajayi <ajayioisaac@gmail.com>
2022-10-12 18:41:56 +0100

    get_op_func.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -3,12 +3,13 @@
 #include <stdio.h>
 
 /**
- *  * main - ...
- *   * @argc: ...
- *    * @argv: ...
- *     *
- *      * Return: ...
- *       */
+ * main - ...
+ * @argc: ...
+ * @argv: ...
+ *
+ * Return: ...
+ */
+
 int main(int argc, char *argv[])
 {
 	int (*oprt)(int, int);
@@ -20,6 +21,7 @@ int main(int argc, char *argv[])
 	}
 
 	oprt = get_op_func(argv[2]);
+
 	if (!oprt)
 	{
 		printf("Error\n");
